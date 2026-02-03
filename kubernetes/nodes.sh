37e0e3ed05fc375f4d6bdd4c4084bfb4d1ce7be9
francislance <francislance@codectl.org>
2026-02-03 11:49:35 +0800

    nodes

---
+++ kubernetes/nodes.sh
@@ -0,0 +1,4 @@
+# get node IPs and custom roles
+kubectl get nodes -o go-template='{{"NAME\tINTERNAL-IP\tROLES\n"}}{{range .items}}{{.metadata.name}}{{"\t"}}{{range .status.addresses}}{{if eq .type "InternalIP"}}{{.address}}{{end}}{{end}}{{"\t"}}{{- $sep := "" -}}{{- if eq (index .metadata.labels "label/kube_control_plane") "true" -}}{{$sep}}control-plane{{- $sep = "," -}}{{- end -}}{{- if eq (index .metadata.labels "label/infra_node") "true" -}}{{$sep}}infra{{- $sep = "," -}}{{- end -}}{{- if eq (index .metadata.labels "label/kube_node") "true" -}}{{$sep}}worker{{- $sep = "," -}}{{- end -}}{{- if eq $sep "" -}}unknown{{- end -}}{{"\n"}}{{end}}' \
+| column -t -s $'\t'
+
